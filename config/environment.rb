97150bc1025da7c26e74109e7d56dcbd672b8720
Dan McGrady <dan@dmix.ca>
2009-05-31 20:30:29 -0400

    README + few misc changes

--- config/environment.rb
+++ config/environment.rb
@@ -9,5 +9,5 @@ Rails::Initializer.run do |config|
     :session_key => '_schedule_session',
     :secret      => 'c0be1ba87a37440e328797d0ab72f0e7b6597c38dcaf1a30d5bd2aa7c397744cab107fc7a3cd5d5030a0c736e043f7a66387730914291975a69922a62a48691d'
   }
-  config.gem "calendar_date_select"
+  #config.gem "calendar_date_select"
 end
