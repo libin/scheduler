2b60d300bd36f988a68b974d268aa2b47ef30269
Dan McGrady <dan@dmix.ca>
2009-06-01 10:53:53 -0400

    logo centered and timezone = EST

--- config/environment.rb
+++ config/environment.rb
@@ -4,7 +4,7 @@ RAILS_GEM_VERSION = '2.2.2' unless defined? RAILS_GEM_VERSION
 require File.join(File.dirname(__FILE__), 'boot')
 Rails::Initializer.run do |config|
 
-  config.time_zone = 'UTC'
+  config.time_zone = 'Eastern Time (US & Canada)'
   config.action_controller.session = {
     :session_key => '_schedule_session',
     :secret      => 'c0be1ba87a37440e328797d0ab72f0e7b6597c38dcaf1a30d5bd2aa7c397744cab107fc7a3cd5d5030a0c736e043f7a66387730914291975a69922a62a48691d'
